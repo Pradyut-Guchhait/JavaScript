8cd630491aecb0bbe1dbae0c033b396e1f582cc3
Pradyut Guchhait <pradyutguchhait04@gmail.com>
2024-08-31 17:53:38 +0530

    Day 1

---
+++ 01test.js
@@ -0,0 +1,15 @@
+let fname = "Pradyut";
+var lname = "Guchhait"
+const RegNo = 229303182;
+
+lname="UN"; //In JavaScript without mentioning data type it will still update the value
+
+/*
+Never use var as it has issues with block scope 
+and
+functional scope
+*/
+
+console.table([fname,lname,RegNo]);
+
+
---
+++ 02Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treats all JS code as newer Version
+// alert(3+3) it will not work as we are using node js
+//Code Readability should be good
+
+let Name = "Pradyut" //string
+let age = 20 //number (2^53) bigint for bigger numbers
+let isloggedin = false //boolen true/false
+
+/* 
+Datatypes:
+ null => standalone value
+ undefined  =>  variable is declared but not assigned any value
+ symbol => Unique
+ object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
